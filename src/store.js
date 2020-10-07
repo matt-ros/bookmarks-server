01a6864c0dbd8ee3ed301aacc9142f83d4033b51
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-10-06 22:02:56 -0400

    Implement full functionality per Checkpoint 10

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+  id: 1,
+  title: 'Fake Website',
+  url: 'http://www.fakewebsite.com',
+  description: 'This is a fake bookmark',
+  rating: 2
+}]
+
+module.exports = bookmarks;
